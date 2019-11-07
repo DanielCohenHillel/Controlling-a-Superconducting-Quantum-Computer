36844a180887223b9f4dbd0284652e559bdaed5f
Daniel Cohen Hillel <danielgch98@gmail.com>
2019-11-07 15:51:07 +0200

    Added article references, more FALSE! results, changed some t

--- Codes/transmon-cavity_gates_GRAPE.py
+++ Codes/transmon-cavity_gates_GRAPE.py
@@ -17,7 +17,7 @@ results_folder = "/transmon-cavity_results"
 # Constants
 h_bar = 1
 
-cavity_levels = 3
+cavity_levels = 8
 
 # Basic operators
 # Atom(a and a-dagger)
@@ -28,11 +28,11 @@ c = qt.tensor(qt.qeye(2), qt.destroy(cavity_levels))
 cd = c.dag()
 
 # Simulation time and accuracy details
-num_time_steps = 50  # Number of time states for the simulation
+num_time_steps = 200  # Number of time states for the simulation
 total_time = 5  # Total simulation time
 dt = total_time/num_time_steps  # Time step size
 times = np.linspace(0.0, total_time, num_time_steps)  # Time space array
-num_grape_iter = 400
+num_grape_iter = 1000
 # Frequencies
 w_a = 1  # Atom(qubit) frequency
 w_c = 1  # Cavity frequency
@@ -41,7 +41,7 @@ g = 1  # Linear photon number dependent dispersive shifts
 # Hamiltonians (Linear/Harmonic)
 H_a = w_a*a*ad  # Atom hamiltonian
 H_c = w_c*c*cd  # Cavity hamiltonian
-H_ac = g * (cd * a + c * ad)  # Atom-Cavity interaction hamiltonian
+H_ac = g * (cd * c * ad * a)  # Atom-Cavity interaction hamiltonian
 H_ac = g * ad*a*cd*c  # Atom-Cavity interaction hamiltonian
 H0 = H_a + H_c + H_ac  # Full atom-cavity hamiltonian w/o a derive
 # Drive hamiltonians
@@ -60,7 +60,8 @@ bl = qt.Bloch()
 def get_drive_fields_grape(operator, disp_graphs=False):
     start_time = time.time_ns()
 
-    result = qt.control.grape_unitary(operator, H0, H_d, num_grape_iter, times)
+    U_start = np.random.random([4, num_time_steps])/5.0
+    result = qt.control.grape_unitary(operator, H0, H_d, num_grape_iter, times, u_start=U_start)
 
     # Atom(transmon)
     QI_a = result.u[-1, 0, :]
@@ -91,8 +92,10 @@ def get_drive_fields_grape(operator, disp_graphs=False):
     return QI_a, QQ_a, QI_c, QQ_c
 
 
-def run_operator(state, QI_a, QQ_a, QI_c, QQ_c):
+def run_operator(state, QI_a, QQ_a, QI_c, QQ_c, show_fid_graph=False):
     start_time = time.time_ns()
+
+    fid = []
 
     prod = 1
     for k in range(num_time_steps):
@@ -100,20 +103,27 @@ def run_operator(state, QI_a, QQ_a, QI_c, QQ_c):
         U = ((-1j * dt) * H).expm()
         prod = U * prod
         bl.add_states(qt.ptrace(prod * state, 0))
+        fid.append(qt.fidelity(prod*state, state_target))
 
     result = prod * state
+
+    if show_fid_graph:
+        fig, ax = plt.subplots(1, 1)
+        ax.plot(times, fid)
+        ax.set_title("Fidelity Over Time")
+        ax.set_xlabel('Time (sec)')
+        ax.set_ylabel('Fidelity')
 
     print("-    Simulating the system took", str((time.time_ns() - start_time) * 1e-9)[0:4], "Seconds")
     return result
 
 
-state_target = qt.tensor(qt.basis(2, 1), qt.basis(cavity_levels, 0))
+state_init = qt.tensor(qt.basis(2, 0), qt.basis(cavity_levels, 0))
+state_target = qt.tensor(qt.basis(2, 0), qt.basis(cavity_levels, 1))
 
 QI_a, QQ_a, QI_c, QQ_c = get_drive_fields_grape(state_target, disp_graphs=True)
 
-state_init = qt.tensor(qt.basis(2, 0), qt.basis(cavity_levels, 0))
-
-state_final = run_operator(state_init, QI_a, QQ_a, QI_c, QQ_c)
+state_final = run_operator(state_init, QI_a, QQ_a, QI_c, QQ_c, show_fid_graph=True)
 
 print("-    Final fidelity:", str(qt.fidelity(state_target, state_final)*100)[0:9] + "%")
 
@@ -122,8 +132,8 @@ bl.add_states(qt.ptrace(state_final, 0))
 bl.show()
 
 xvec = np.linspace(-5, 5, 500)
-W_init = qt.wigner(state_init, xvec, xvec)
-W_final = qt.wigner(state_final, xvec, xvec)
+W_init = qt.wigner(qt.ptrace(state_init, 1), xvec, xvec)
+W_final = qt.wigner(qt.ptrace(state_final, 1), xvec, xvec)
 
 fig, axes = plt.subplots(1, 2)
 
@@ -135,5 +145,8 @@ axes[1].set_title("Final")
 
 fig.tight_layout()
 plt.show()
+
+print("\n--\nInit: " + str(state_init) + "\n")
+print("Final: " + str(state_final) + "\n--\n")
 
 print("\n-------------------------------------------------------------------------------------------------------------")
